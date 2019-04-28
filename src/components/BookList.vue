befd2f70c67ca2d6b162e153a22ee25af9befbc8
Jeffrey Wolff <jeff.wolff@spellingcity.com>
2019-04-28 11:20:44 -0400

    added bookform
    add/set up bookform

--- src/components/BookList.vue
+++ src/components/BookList.vue
@@ -4,16 +4,20 @@
     <ul>
       <book-item v-for="book in books" :book='book'></book-item>
     </ul>
+    <hr>
+    <book-form @addBook='appendBook'></book-form>
   </div>
 </template>
 
 <script>
   import BookItem from './BookItem';
+  import BookForm from './BookForm';
 
   export default {
     name: 'BookList',
     components: {
-      BookItem
+      BookItem,
+      BookForm
     },
     data() {
       return {
@@ -23,6 +27,11 @@
           {title: 'American Gods', author: 'Neil Gaiman'},
           {title: 'Amusing Ourselves to Death', author: 'Neil Postman'},
         ]
+      }
+    },
+    methods: {
+      appendBook(bookTitle , bookAuthor) {
+        this.books.push({title: bookTitle, author:bookAuthor})
       }
     }
   }
